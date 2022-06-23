f25c6532851dc801474084e75ab1c9b3b6a0cc5f
jum0624 <jum0624@naver.com>
2022-06-23 17:27:14 +0900

    feat: 회원 도메인과 레포지토리 추가

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무애서는 동시성 문제가 발생할 수 있음. because -> static
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  // null일 경우 optional로 감싸서 반환
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()  // store에 있는 모든 값들을 루프
+                .filter(member -> member.getName().equals(name)) // 만약 파라미터로 넘어온 name값과 같다면 필터링
+                .findAny();  // 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
