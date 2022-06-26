e0294749933654d7840ac0586faba57869be7739
jum0624 <jum0624@naver.com>
2022-06-27 03:25:57 +0900

    chore: dataSource에 대한 주석 설명추가

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -12,8 +12,7 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-
+    private final DataSource dataSource;  // dataSource 는 데이터베이스 커넥션을 획득할 때 사용하는 객체.
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
