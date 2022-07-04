41d5855e38e19c3439cba403d503a918b7e7f551
jum0624 <jum0624@naver.com>
2022-07-04 20:58:48 +0900

    add: AppConfig 리팩터링
    각 역할에 따른 구현이 잘 보이도록 설계

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,29 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-public class AppConfig {
+public class AppConfig {  // 역할들이 드러나는 것이 중요!!
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());  // 생성자 주입
+    public MemberService memberService() {  // 멤버 서비스의 역할
+        return new MemberServiceImpl(MemberRepository());  // 생성자 주입
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+    private MemberRepository MemberRepository() {  // 멤버 레포지토리의 역할
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){  // 주문 서비스의 역할
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {  // 주문 정책의 역할
+        return new FixDiscountPolicy();
     }
 }
