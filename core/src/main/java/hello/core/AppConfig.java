f636b9165f83dc0d1ce3ab8afd3877a16b3d8066
jum0624 <jum0624@naver.com>
2022-07-05 00:53:35 +0900

    style: Appconfig를 spring으로 전환

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {  // 역할들이 드러나는 것이 중요!!
 
+    @Bean
     public MemberService memberService() {  // 멤버 서비스의 역할
         return new MemberServiceImpl(MemberRepository());  // 생성자 주입
     }
 
-    private MemberRepository MemberRepository() {  // 멤버 레포지토리의 역할
+    @Bean
+    public MemberRepository MemberRepository() {  // 멤버 레포지토리의 역할
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){  // 주문 서비스의 역할
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {  // 주문 정책의 역할
         return new RateDiscountPolicy();
     }
