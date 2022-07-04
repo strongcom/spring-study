a7a2dd86dfc5d21980f52868e161c9fdfe13fff7
jum0624 <jum0624@naver.com>
2022-07-04 17:37:02 +0900

    add: JPA

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+    //PK값
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 알아서 PK값 정해줌 (Identity)
     private Long id;
+    @Column(name = "username")  //column 이름 : username
     private String name;
+
 
     public Long getId() {
         return id;
